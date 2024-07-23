ab986da8d172597c2ca3f33573b0d7cfe07e81f9
302A-17\cre <a>
2024-07-23 15:22:43 +0900

    work3 終わり

--- works/work3/q1.php
+++ works/work3/q1.php
@@ -2,7 +2,7 @@
 
 // 引数として受け取った数値を2倍にして返す関数を完成させなさい。
 
-double(5);
+$a = double(9);
 echo $a;
 function double($x)
 {
--- works/work3/q3.php
+++ works/work3/q3.php
@@ -2,11 +2,18 @@
 
 // 配列を引数として受け取り、その配列内の数値の平均値を計算して返す関数を完成させなさい
 
-$values = array(1, 2, 3, 4, 5);
-$result = average();
+$values = array(1, 2, 3);
+$result = average($values);
 echo "平均値: " . $result . "\n";
 
 
-function average()
+function average($value)
 {
+    $goukei = 0;
+    for ($i = 0; $i < count($value); $i++) {
+
+        $goukei = $goukei + $value[$i];
+    }
+    $heikin = $goukei / $i;
+    return $heikin;
 }
